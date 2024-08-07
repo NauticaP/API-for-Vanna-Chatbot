5144037f6a5aca147770641ee4d27d4a9294506d
Luis Rivas <lmrp89@gmail.com>
2024-08-07 10:03:19 +0200

    vanna_service.py

--- app/services/vanna_service.py
+++ app/services/vanna_service.py
@@ -1,8 +1,18 @@
 from vanna.remote import VannaDefault
 import psycopg2
 from psycopg2 import OperationalError
+import os
+from dotenv import load_dotenv
 
-def create_connection(db_name="db", db_user="user", db_password="password", db_host="host", db_port="port"):
+load_dotenv()  # Carga las variables de entorno desde el archivo .env
+
+def create_connection():
+    db_name = os.getenv('DB_NAME')
+    db_user = os.getenv('DB_USER')
+    db_password = os.getenv('DB_PASSWORD')
+    db_host = os.getenv('DB_HOST')
+    db_port = os.getenv('DB_PORT')
+
     connection = None
     try:
         connection = psycopg2.connect(
@@ -18,8 +28,8 @@ def create_connection(db_name="db", db_user="user", db_password="password", db_h
     return connection
 
 def setup_vanna():
-    api_key = 'Vanna API Key'
-    model = 'Vanna Model'
+    api_key = os.getenv('VANNA_API_KEY')
+    model = os.getenv('VANNA_MODEL')
 
     connection = create_connection()
 
@@ -27,7 +37,7 @@ def setup_vanna():
         vn = VannaDefault(api_key=api_key, model=model)
         try:
             conn_info = connection.get_dsn_parameters()
-            vn.connect_to_postgres(host=conn_info['host'], dbname=conn_info['dbname'], user=conn_info['user'], password='1234', port=conn_info['port'])
+            vn.connect_to_postgres(host=conn_info['host'], dbname=conn_info['dbname'], user=conn_info['user'], password=db_password, port=conn_info['port'])
             print("Vanna connected successfully to the database")
         except Exception as e:
             print(f"Error connecting Vanna to the database: {e}")
